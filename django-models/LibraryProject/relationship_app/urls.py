888177dc12d730f1ed3eebb3986d9f2ba18ec677
Ammon Kering <keringammon@gmail.com>
2025-11-10 00:45:20 +0300

    added user views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
@@ -9,4 +12,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin-dashboard/', admin_view, name='admin_dashboard'),
+    path('librarian-dashboard/', librarian_view, name='librarian_dashboard'),
+    path('member-dashboard/', member_view, name='member_dashboard'),
 ]
