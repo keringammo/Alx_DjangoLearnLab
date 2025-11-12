8028d6151afd6fbd9001f622717153484727f99e
Ammon Kering <keringammon@gmail.com>
2025-11-12 22:49:17 +0300

    corrections

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,21 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book
 
+# Book admin
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_year")
+    list_display = ("title", "author", "publication_year", "owner")
     search_fields = ("title", "author")
     list_filter = ("publication_year",)
 
-@admin.register(CustomUser)
+# CustomUser admin
 class CustomUserAdmin(UserAdmin):
-    # Fields to display in the user list view
     list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active', 'is_superuser')
     search_fields = ('username', 'email')
     ordering = ('username',)
 
-    # Fields to show in the detail/edit view
     fieldsets = (
         (None, {'fields': ('username', 'email', 'password')}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
@@ -24,10 +23,12 @@ class CustomUserAdmin(UserAdmin):
         ('Important Dates', {'fields': ('last_login', 'date_joined')}),
     )
 
-    # Fields to show in the add user form
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active')}
         ),
     )
+
+# ⚠️ Explicit registration
+admin.site.register(CustomUser, CustomUserAdmin)
