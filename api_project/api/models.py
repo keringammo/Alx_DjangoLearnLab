4096c9e894f48ebc50c27513f695778e700ea4f5
Ammon Kering <keringammon@gmail.com>
2025-11-23 00:05:31 +0300

    corrections

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,9 @@
+# api/models.py
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)   # Book title
+    author = models.CharField(max_length=100)  # Author name
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
