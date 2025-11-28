c03cb6b5fd47e84275e392359e26c12e473e7e45
Ammon Kering <keringammon@gmail.com>
2025-11-28 21:57:11 +0300

    corrections

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,39 +1,33 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # read allowed, write blocked
 
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    # Custom behavior: validate inputs before creation
-    def perform_create(self, serializer):
-        serializer.is_valid(raise_exception=True)  # custom validation
-        serializer.save()  # add logging, filters, etc if needed
+    permission_classes = [IsAuthenticated]  # only authenticated users can create
 
 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    # Custom behavior: validation + update rules
-    def perform_update(self, serializer):
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+    permission_classes = [IsAuthenticated]  # only authenticated users can update
 
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]  # only authenticated users can delete
