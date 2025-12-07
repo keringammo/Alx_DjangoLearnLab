78cc55a3621051226788dd885f1cafe298aec6a2
Ammon Kering <keringammon@gmail.com>
2025-12-07 19:00:09 +0300

    CORRECTION

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,51 +1,43 @@
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
 
-# List view (public)
 class PostListView(ListView):
     model = Post
     template_name = 'posts/post_list.html'
     context_object_name = 'posts'
     ordering = ['-created_at']
 
-# Detail view (public)
 class PostDetailView(DetailView):
     model = Post
     template_name = 'posts/post_detail.html'
 
-# Create view (authenticated users only)
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'posts/post_form.html'
 
     def form_valid(self, form):
-        # Automatically set author to logged-in user
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-# Update view (only author)
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'posts/post_form.html'
 
     def test_func(self):
-        # Only allow post author to edit
         post = self.get_object()
         return self.request.user == post.author
 
-# Delete view (only author)
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'posts/post_confirm_delete.html'
     success_url = reverse_lazy('post-list')
 
     def test_func(self):
-        # Only allow post author to delete
         post = self.get_object()
         return self.request.user == post.author
-
