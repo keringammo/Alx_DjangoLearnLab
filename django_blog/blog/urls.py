b40fd048a2013e6282352949d31881fe07a2bfe1
Ammon Kering <keringammon@gmail.com>
2025-12-07 15:12:02 +0300

    Add URL patterns for Blog Post CRUD views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,18 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    # List all posts
     path('posts/', PostListView.as_view(), name='post-list'),
-
-    # Create a new post
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
-
-    # View a single post
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-
-    # Edit a post
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-
-    # Delete a post
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
