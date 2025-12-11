9e64fc51f10ff73db21722d6fb8de9ef5e6c1df8
Ammon Kering <keringammon@gmail.com>
2025-12-11 23:21:46 +0300

    CORRECTION

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.generics import get_object_or_404
+from rest_framework.generics import get_object_or_404  # ✅ Import from DRF
 from .models import Post, Like
 from notifications.models import Notification
 from django.contrib.contenttypes.models import ContentType
