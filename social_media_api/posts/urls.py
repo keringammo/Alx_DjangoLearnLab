5a00071adecfe9c197ef9eb0bdfe793110a2cb6f
Ammon Kering <keringammon@gmail.com>
2025-12-11 23:20:16 +0300

    CORRECTION

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import LikePostView, UnlikePostView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView
 
-urlpatterns += [
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'comments', CommentViewSet, basename='comment')
+
+urlpatterns = [
+    path('', include(router.urls)),
     path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
     path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
