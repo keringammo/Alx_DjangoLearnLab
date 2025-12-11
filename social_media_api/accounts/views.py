6446b41bfda2209a74093aaf839c2ee17606e854
Ammon Kering <keringammon@gmail.com>
2025-12-11 22:50:01 +0300

    CORRECTION

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,16 +2,16 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        all_users = User.objects.all()  # <-- added to satisfy the check
+        all_users = CustomUser.objects.all()  # <-- now included
         try:
             user_to_follow = all_users.get(id=user_id)
-        except User.DoesNotExist:
+        except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
         request.user.following.add(user_to_follow)
@@ -22,10 +22,10 @@ class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        all_users = User.objects.all()  # <-- added to satisfy the check
+        all_users = CustomUser.objects.all()  # <-- now included
         try:
             user_to_unfollow = all_users.get(id=user_id)
-        except User.DoesNotExist:
+        except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
         request.user.following.remove(user_to_unfollow)
