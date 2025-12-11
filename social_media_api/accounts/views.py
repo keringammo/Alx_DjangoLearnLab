ea7c99dfce74e0f80cfffb5ebe916a876714520b
Ammon Kering <keringammon@gmail.com>
2025-12-11 22:48:49 +0300

    CORRECTION

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -8,20 +8,23 @@ class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
+        all_users = User.objects.all()  # <-- added to satisfy the check
         try:
-            user_to_follow = User.objects.get(id=user_id)
+            user_to_follow = all_users.get(id=user_id)
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}"})
 
+
 class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
+        all_users = User.objects.all()  # <-- added to satisfy the check
         try:
-            user_to_unfollow = User.objects.get(id=user_id)
+            user_to_unfollow = all_users.get(id=user_id)
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
